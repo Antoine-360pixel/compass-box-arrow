dece4234e9bb1068e0869442c8247b7eedf461e5
Antoine <antoine.nnc@gmail.com>
2013-06-28 08:01:53 +0200

    change grunt file to acces the server from outside

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,7 +50,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                hostname: '192.168.0.49'
             },
             livereload: {
                 options: {
@@ -85,7 +85,7 @@ module.exports = function (grunt) {
         },
         open: {
             server: {
-                path: 'http://localhost:<%= connect.options.port %>'
+                path: 'http://192.168.0.49:<%= connect.options.port %>'
             }
         },
         clean: {
